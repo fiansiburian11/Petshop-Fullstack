7ffea23639eac0f3b3c8307871959dd999b103bb
fiansiburian11 <bukanfirman11@gmail.com>
2025-06-25 18:18:49 +0700

    update kembali

--- src/app/api/keranjang/route.ts
+++ src/app/api/keranjang/route.ts
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic";
+export const runtime = "nodejs";     
 
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
