14656b64b8ee4f1fb05c12291205ae5fcb7efc4c
fiansiburian11 <bukanfirman11@gmail.com>
2025-06-25 21:38:35 +0700

    update responsive design

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import Navbar from "./Navbar";
 import WelcomeScetion from "./WelcomeSection";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -10,7 +11,9 @@ const HeroSection = () => {
       <div className="relative w-full h-[500px]">
         <Image src="/images/Rectangle.png" alt="rectangle" fill className="z-0" />
         <div className="absolute top-5 left-5 text-white text-lg font-extrabold z-10 ">
+          <Link href="/">
           <Image src="/images/logo.png" alt="logo" width={125} height={125} className="text-black" />
+          </Link>
         </div>
         <Navbar />
         <WelcomeScetion />
